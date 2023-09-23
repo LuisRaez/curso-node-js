7a2b639a8f00e005a37d12e10b69ba153e45c680
LuisRaez <luis.raez96@gmail.com>
2023-09-22 20:14:26 -0500

    Finalización Clase-2

--- Clase-1/3.fs-readFile.js
+++ Clase-1/3.fs-readFile.js
@@ -1,13 +1,13 @@
 const fs = require('node:fs')
 
 console.log('Leyendo el primer archivo...')
-fs.readFile('./archivo.txt', 'utf-8', (erro, text) => { // <---- ejecutas este callback
+fs.readFile('./archivo.txt', 'utf-8', (err, text) => { // <---- ejecutas este callback
   console.log('primer texto:', text)
 })
 
 console.log('--> Hacer cosas mientras lee el archivo...')
 
 console.log('Leyendo el segundo archivo...')
-fs.readFile('./archivo2.txt', 'utf-8', (erro, text) => {
+fs.readFile('./archivo2.txt', 'utf-8', (err, text) => {
   console.log('segundo texto:', text)
 })
